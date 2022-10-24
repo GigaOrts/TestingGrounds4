dbe3383e6b94593c6987e35bc5d5fb3d368618d3
GigaOrts <ortsortskhoev95@gmail.com>
2022-10-24 15:08:00 +0300

    Hard Enemies Added

--- Assets/Course Library/Scripts/Spawner.cs
+++ Assets/Course Library/Scripts/Spawner.cs
@@ -3,19 +3,16 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     private const float SpawnRange = 9f;
+    private const int randomizeValue = 5;
 
     public int waveNumber = 1;
     public GameObject enemyPrefab;
+    public GameObject hardEnemyPrefab;
     public GameObject powerupPrefab;
 
     private Vector3 powerupOffset = new Vector3(0f, 0.5f, 0f);
     private int enemyCount;
 
-    private void Start()
-    {
-        SpawnEnemyWave(waveNumber);
-        Instantiate(powerupPrefab, GenerateSpawnPoint() + powerupOffset, powerupPrefab.transform.rotation);
-    }
     private void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
@@ -31,7 +28,17 @@ public class Spawner : MonoBehaviour
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+            int randomIndex = Random.Range(0, randomizeValue + 1);
+            bool canSpawnHardEnemy = randomIndex % randomizeValue == 0;
+
+            if (canSpawnHardEnemy)
+            {
+                Instantiate(hardEnemyPrefab, GenerateSpawnPoint(), hardEnemyPrefab.transform.rotation);
+            }
+            else
+            {
+                Instantiate(enemyPrefab, GenerateSpawnPoint(), enemyPrefab.transform.rotation);
+            }
         }
 
         waveNumber++;
